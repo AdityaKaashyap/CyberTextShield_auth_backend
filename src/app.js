b2a71b31bf1f2673c9e74d2dfc63f571098ebcf8
AdityaKaashyap <adityapk333@gmail.com>
2025-12-07 13:05:41 +0530

    jwt updated

--- src/app.js
+++ src/app.js
@@ -6,15 +6,19 @@ const authRoutes = require("./routes/auth.routes");
 const permRoutes = require("./routes/permission.routes");
 const smsRoutes = require("./routes/sms.routes");
 const reportsRoutes = require("./routes/report.routes");
+const authMiddleware = require("./middleware/auth.middleware");
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 
 app.use("/auth", authRoutes);
-app.use("/permissions", permRoutes);
-app.use("/sms", smsRoutes);
-app.use("/reports", reportsRoutes);
+
+app.use("/reports", authMiddleware, reportRoutes);
+app.use("/sms", authMiddleware, smsRoutes);
+app.use("/permissions", authMiddleware, permissionRoutes);
+
+app.get("/", (req, res) => res.send("API Working"))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Auth backend running on ${PORT}`));
