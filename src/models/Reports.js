9de3d0dc092e2313d515b38dc95874e76e22b3ad
AdityaKaashyap <adityapk333@gmail.com>
2025-12-07 11:11:58 +0530

    Updated false positive

--- src/models/Reports.js
+++ src/models/Reports.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const ReportSchema = new mongoose.Schema({
-  type: { type: String }, // false_positive / false_prediction
-  payload: { type: Object },
+  type: { type: String, enum: ["false_positive", "false_prediction"], required: true },
+
+  message: { type: String, required: true },
+  sender: { type: String, default: "UNKNOWN" },
+
+  predicted_label: { type: String, required: true },  // what model predicted
+  correct_label: { type: String, required: true },    // what user says it actually is
+
   reported_at: { type: Date, default: Date.now }
 });
 
